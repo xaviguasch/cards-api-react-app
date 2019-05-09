f0aaa114b757a5e452d6bfa7b2c104815457cb5d
xaviguasch <xaviguasch@gmail.com>
2019-05-09 11:53:33 +0200

    added random transformation effect for each Card component

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Card from './Card'
+import './Deck.css'
 import axios from 'axios'
 
 const API_BASE = 'https://deckofcardsapi.com/api/deck'
@@ -50,7 +51,7 @@ class Deck extends Component {
       <div>
         <h1>Deck</h1>
         <button onClick={this.getCard}>Get Card</button>
-        {cards}
+        <div className='Deck-cardarea'>{cards}</div>
       </div>
     )
   }
